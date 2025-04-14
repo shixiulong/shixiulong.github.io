5164b1f3c1e69d03fc44109ee379c7f5b3da7273
蓝色 <40845795+shixiulong@users.noreply.github.com>
2025-04-14 16:24:10 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import '@mdi/font/css/materialdesignicons.css'
-
+// import '@mdi/font/css/materialdesignicons.css'
+// import './assets/font-family/index.css'
 import 'lanse/styles'
 import { createVuetify } from 'lanse'
 import * as components from 'lanse/components'
